4c10119af6a5be50b0b6660cfe72febbbf2a5111
prrknh <prrknh@gmail.com>
2019-09-01 01:26:21 +0900

    add http request logging

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ func main() {
 
 	http.HandleFunc("/create", handler.CreateContainer(image))
 	http.HandleFunc("/stop", handler.StopContainer())
-	http.ListenAndServe(":5000", nil)
+	log.Println("============================================")
+	log.Println("============= start server =================")
+	log.Println("============================================")
+	http.ListenAndServe(":5000", logRequest(http.DefaultServeMux))
 }
 
 func initialize() (image string) {
@@ -41,4 +44,11 @@ func initialize() (image string) {
 	}
 	log.Fatalf("\"%s\" image not found", image)
 	return
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
